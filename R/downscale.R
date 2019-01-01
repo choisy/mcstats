130c7521523e0afc34b2fb7550eca3d287d82bce
Marc Choisy <marc.choisy@ird.fr>
2019-01-01 14:19:07 +0700

    Replace = by <- in downscale

--- R/downscale.R
+++ R/downscale.R
@@ -80,7 +80,7 @@ downscale <- function(y, x, n, interval = NULL) {
 
   nb <- length(x)
   the_centers <- centers(x, TRUE)
-  if (is.null(interval)) interval = 3 * range(y)
+  if (is.null(interval)) interval <- 3 * range(y)
 
   # The function that makes the list of linear models (one per interval).
   # Note that it uses `nb` and `the_centers` defined above.
